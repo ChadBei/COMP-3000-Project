737ed0fe73aab39b03a3062fa239de5670bd366c
Chad Beibide <cbeibide@bellsouth.net>
2020-11-07 16:59:19 -0600

    Updated main()
    Signed-off-by: Chad Beibide <cbeibide@bellsouth.net>

--- main.cpp
+++ main.cpp
@@ -2,8 +2,25 @@
 // Created by Chad Beibide and Jackson Houston
 //
 
-#include "main.h"
+#include <iomanip>
+#include <time.h>
+
+int[] mergeSort(int[] a);
+int[] quickSort(int[] a);
+int[] bubbleSort(int[] a);
+int[] insertionSort(int[] a);
+int[] blockSort(int[] a);
+int[] slowSort(int[] a);
+
 
 void main(){
     cout << "Group Project" << endl;
+
+    Time startTime, endTime;
+
+    mergeSort(Array);
+
+
+
+
 }
